2401645885551fd7e033c4ec80b3acad76a5d1ba
dextromepam <sergio@laszal.xyz>
2023-08-15 21:10:56 +0200

    add true/false

---
+++ src/false.go
@@ -0,0 +1,11 @@
+// false.go
+// author sergio@laszal.xyz
+// date 15/8/2023
+
+package main
+
+import "os"
+
+func main() {
+	os.Exit(1)
+}
---
+++ src/true.go
@@ -0,0 +1,11 @@
+// true.go
+// author sergio@laszal.xyz
+// date 15/8/2023
+
+package main
+
+import "os"
+
+func main() {
+	os.Exit(0)
+}
